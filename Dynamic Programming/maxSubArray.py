8adc171852654a1bc3ddafb0d2a80cefd6c70ce8
Fernanda Guzman Gomez <gzmanfer@outlook.com>
2020-11-08 22:45:48 -0600

    03 Commit

--- Dynamic Programming/maxSubArray.py
+++ Dynamic Programming/maxSubArray.py
@@ -1,3 +1,19 @@
+'''
+Maximum Subarray
+
+@ What is the problem?
+  Find the contiguous subarray with the maximum sum...
+
+@ What is a Contiguous Subarray?
+  Could be a single number [1], a pair [1,-3] or even the whole array [1, -3, 2, 1, -1],
+  but e.g. [-3,1] can't be a contiguous subarray because it is not contiguous.
+
+@ Kadane's Algorithm
+  Is the famous solution, and also the most optimum because is O(n).
+  Explanation: https://www.youtube.com/watch?v=58yMrWfUS7k
+
+'''
+
 
 def maxSubArray(nums):
     
@@ -12,27 +28,13 @@ def maxSubArray(nums):
 
 
 
-#! TEST __________________________________________________________________________________
+# TEST __________________________________________________________________________________
 
 nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-
 print(maxSubArray(nums))
 
 
+# Input:        nums = [-2,1,-3,4,-1,2,1,-5,4]
+# Output:       6
 
-# NOTES __________________________________________________________________________________
-
-# What is a Contiguous Subarray?
-# Could be a single number [1], a pair [1,-3] or even the whole array [1, -3, 2, 1, -1],
-# but e.g. [-3,1] can't be a contiguous subarray because it is not contiguous.
-
-# What is the problem?
-# "Find the contiguous subarray with the maximum sum"
-
-# Bute Force Solution:
-# Checking all the possible subarrays and picking the one with the maximum sum.
-# It is a possible solution, but it takes a O(n^2) time.
-
-# Optimal Solution:
-# Kadane's Algorithm is the famous solution, and also the most optimum because is O(n).
-# Explanation: https://www.youtube.com/watch?v=58yMrWfUS7k
+# Explanation:  [4,-1,2,1] has the largest sum = 6.

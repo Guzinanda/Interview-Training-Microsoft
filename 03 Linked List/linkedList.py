8742ba42f17fa7b1b26e5d70f61bfee84f9a447f
Fernanda Guzman Gomez <gzmanfer@gmail.com>
2021-01-11 10:25:18 -0600

    Commit

--- 03 Linked List/linkedList.py
+++ 03 Linked List/linkedList.py
@@ -1,82 +1,35 @@
 '''
-A Linked List is a Data Structure for sorting a collection of items.
-It does it in a different way from the way an array does it.
+@   Delete Node in a Linked List
 
-An array can be visualized as a long box whit many partitions of the same size:
-[6|3|4|2|1]
+@   Link:
+    https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/553/
+ 
+@   Instructions:
+    Write a function to delete a node in a singly-linked list. You will not be given access to the 
+    head of the list, instead you will be given access to the node to be deleted directly.
 
-A linked list can be visualized as many boxes that are connected to each other:
-[6]->[3]->[4]->[2]->[1]
+    It is guaranteed that the node to be deleted is not a tail node in the list.
 
+@   Example 01:
+    Input: head = [4,5,1,9], node = 5
+    Output: [4,1,9]
+    
+    You are given the second node with value 5, the linked list should become 
+    4 -> 1 -> 9 after calling your function.
 '''
 
-class node:
-    def __init__(self, data=None):
-        self.data = data
-        self.next = None
 
-class linked_list:
-    def __init__(self):
-        self.head = node()
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
-    def append(self, data):
-        new_node = node(data)
-        cur = self.head
-        while cur.next != None:
-            cur = cur.next
-        cur.next = new_node
-    
-    def length(self):
-        cur = self.head
-        total = 0
-        while cur.next != None:
-            total += 1
-            cur = cur.next
-        return total
-    
-    def display(self):
-        elements = []
-        cur_node = self.head
-        while cur_node.next != None:
-            cur_node = cur_node.next
-            elements.append(cur_node.data)
-            print(elements)
-    
-    def get(self,index):
-        if index >= self.length():
-            print("ERROR: 'Get' Index out of range!")
-            return None
-        cur_idx = 0
-        cur_node = self.head
-        while True:
-            cur_node = cur_node.next
-            if cur_idx == index: 
-                return cur_node.data
-                cur_idx += 1
-    
-    def erase(self, index):
-        if index >= self.length():
-            print("ERROR: 'Erase' Index out of range!")
-            return
-        cur_idx = 0
-        cur_node = self.head
-        while True:
-            last_node = cur_node
-            cur_node = cur_node.next
-            if cur_idx == index:
-                last_node.next = cur_node.next
-                return
-            cur_idx += 1
-
-
-my_list = linked_list()
-
-my_list.append(1)
-my_list.append(2)
-my_list.append(3)
-my_list.append(4)
-my_list.display()
-
-
-my_list.erase(1)
-#my_list.display()
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
